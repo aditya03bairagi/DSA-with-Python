baacc9e55d206f48cc25f38e65cb337c95799084
aditya03bairagi <adityabairagi927@gmail.com>
2026-08-14 18:38:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (19.81%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,3 +1,22 @@
 class Solution:
+    def findPow(self, x, n): # using recursion
+        # base case
+        if n == 0:
+            return 1
+
+        # recursive case
+        a = self.findPow(x, n // 2)
+
+        if n % 2 == 0:
+            return a * a
+        else:
+            return a * a * x
     def myPow(self, x: float, n: int) -> float:
-        return x ** n
+        # return x ** n  # using exponent operator
+        # return pow(x, n) # using pow() function
+
+        # using recursion
+        if n >= 0:
+            return self.findPow(x, n)
+        else:
+            return 1 / self.findPow(x, n * (-1))
